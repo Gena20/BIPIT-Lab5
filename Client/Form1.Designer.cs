9ad9af0ef7775a557d2bf913feb020ee8db6e600
gena <peryatin.gena@yandex.ru>
2020-10-30 23:01:51 +0500

    Updated client form to use service

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -28,13 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvTrips = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCar = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbObjFrom = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbObjTo = new System.Windows.Forms.ComboBox();
+            this.dateFrom = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTo = new System.Windows.Forms.DateTimePicker();
+            this.btnAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTrips)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTrips
+            // 
+            this.dgvTrips.AllowUserToAddRows = false;
+            this.dgvTrips.AllowUserToDeleteRows = false;
+            this.dgvTrips.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTrips.Location = new System.Drawing.Point(12, 12);
+            this.dgvTrips.Name = "dgvTrips";
+            this.dgvTrips.ReadOnly = true;
+            this.dgvTrips.Size = new System.Drawing.Size(776, 236);
+            this.dgvTrips.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "car";
+            // 
+            // cbCar
+            // 
+            this.cbCar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCar.FormattingEnabled = true;
+            this.cbCar.Location = new System.Drawing.Point(71, 257);
+            this.cbCar.Name = "cbCar";
+            this.cbCar.Size = new System.Drawing.Size(121, 21);
+            this.cbCar.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "obj_from";
+            // 
+            // cbObjFrom
+            // 
+            this.cbObjFrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbObjFrom.FormattingEnabled = true;
+            this.cbObjFrom.Location = new System.Drawing.Point(71, 291);
+            this.cbObjFrom.Name = "cbObjFrom";
+            this.cbObjFrom.Size = new System.Drawing.Size(121, 21);
+            this.cbObjFrom.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "obj_to";
+            // 
+            // cbObjTo
+            // 
+            this.cbObjTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbObjTo.FormattingEnabled = true;
+            this.cbObjTo.Location = new System.Drawing.Point(71, 325);
+            this.cbObjTo.Name = "cbObjTo";
+            this.cbObjTo.Size = new System.Drawing.Size(121, 21);
+            this.cbObjTo.TabIndex = 2;
+            // 
+            // dateFrom
+            // 
+            this.dateFrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateFrom.Location = new System.Drawing.Point(71, 359);
+            this.dateFrom.Name = "dateFrom";
+            this.dateFrom.Size = new System.Drawing.Size(121, 20);
+            this.dateFrom.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 361);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "date_from";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 394);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "date_to";
+            // 
+            // dateTo
+            // 
+            this.dateTo.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTo.Location = new System.Drawing.Point(71, 392);
+            this.dateTo.Name = "dateTo";
+            this.dateTo.Size = new System.Drawing.Size(121, 20);
+            this.dateTo.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 422);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(180, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 488);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dateTo);
+            this.Controls.Add(this.dateFrom);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbObjTo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbObjFrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbCar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvTrips);
+            this.Name = "Form1";
+            this.Text = "Trips";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTrips)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvTrips;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbObjFrom;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbObjTo;
+        private System.Windows.Forms.DateTimePicker dateFrom;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTo;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
 
--- Client/Form1.cs
+++ Client/Form1.cs
@@ -12,9 +12,57 @@ namespace Client
 {
     public partial class Form1 : Form
     {
+        ServiceReference1.Service1Client service;
+
         public Form1()
         {
             InitializeComponent();
+
+            service = new ServiceReference1.Service1Client();
+        }
+
+        private void FillComboBox(ComboBox comboBox, string tabelName)
+        {
+            comboBox.DataSource = service.GetSelectData(tabelName);
+            comboBox.DisplayMember = "name";
+            comboBox.ValueMember = "id";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvTrips.DataSource = service.GetData();
+            FillComboBox(cbCar, "cars");
+            FillComboBox(cbObjFrom, "objects");
+            FillComboBox(cbObjTo, "objects");
+        }
+
+        private bool ValidateFields()
+        {
+            if (dateFrom.Value >= dateTo.Value)
+            {
+                MessageBox.Show("dateTo must be more than dateFrom");
+                return false;
+            }
+            if ((int)cbObjFrom.SelectedValue == (int)cbObjTo.SelectedValue)
+            {
+                MessageBox.Show("objTo and objFrom must be different");
+                return false;
+            }
+            return true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (! ValidateFields()) return;
+            service.Insert(
+                (int)cbCar.SelectedValue,
+                (int)cbObjFrom.SelectedValue,
+                (int)cbObjTo.SelectedValue,
+                dateFrom.Value.ToString("yyyy-MM-dd"),
+                dateTo.Value.ToString("yyyy-MM-dd")
+            );
+
+            Form1_Load(null, null);
         }
     }
 }
